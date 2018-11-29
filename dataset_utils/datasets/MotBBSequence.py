cf411609cd566ef381236e57da994194e75c694b
atikin <n.kister@tu-bs.de>
2018-11-29 19:41:57 +0100

    Added train valid split in sequence data and pedestrian apearing mid train sequence are removed

--- dataset_utils/datasets/MotBBSequence.py
+++ dataset_utils/datasets/MotBBSequence.py
@@ -10,7 +10,8 @@ class MotBBSequence(Dataset):
     dataset which loads each frame individual
     """
 
-    def __init__(self, paths_file, loader=default_loader, seq_length=20, new_height=224, new_width=224, step=10):
+    def __init__(self, paths_file, loader=default_loader, seq_length=20, new_height=416, new_width=416, step=5,
+                 valid_ratio=0.2):
         """
         inits of all file names and bounding boxes
         """
@@ -24,7 +25,8 @@ class MotBBSequence(Dataset):
 
         #            dict    list  list
         # tmp format video->frame->boxes with id
-        videos = {i: [] for i in range(len(paths))}
+        videos_train = {i: [] for i in range(len(paths))}
+        videos_valid = {i: [] for i in range(len(paths))}
         # stats stuff
         all_traj_lengths = np.zeros(1)
         all_displacements = np.zeros(1)
@@ -85,34 +87,71 @@ class MotBBSequence(Dataset):
                 # displacement from begin to end
                 start = gt[ids[0], 2:4]
                 end = gt[ids[-1], 2:4]
-                if np.sqrt(np.sum(np.square(start-end))) > 224:
+                if np.sqrt(np.sum(np.square(start-end))) > 416:
                     print(start, end)
                 displacments[ped_id] = np.sqrt(np.sum(np.square(start-end)))
             all_traj_lengths = np.concatenate([all_traj_lengths, traj_lengths])
             all_displacements = np.concatenate([all_displacements, displacments])
 
             # create structure
-            videos[i] = [None for j in range(num_frames)]
-            for j in range(num_frames):
-                videos[i][j] = gt[np.where(gt[:, 0] == j)]
+            # create 10 frame gap between train and valid data
+            train_test_split_idx = int(num_frames * (1.0 - valid_ratio))
+            num_train_frames = train_test_split_idx - 10
+            num_valid_frames = num_frames - train_test_split_idx
+
+            videos_train[i] = [None for j in range(num_train_frames)]
+            for j in range(num_train_frames):
+                videos_train[i][j] = gt[np.where(gt[:, 0] == j)]
+            videos_valid[i] = [None for j in range(num_valid_frames)]
+            for j in range(num_valid_frames):
+                videos_valid[i][j] = gt[np.where(gt[:, 0] == j + train_test_split_idx)]
 
         # [batch_size, seq_index, (id bb)]
         # first idea : assume each frame has at maximum 120 (calc it in stats)
-        self.sequences = []
-        for video in videos.keys():  # todo look up wether .keys returns sorted list
-            video_len = len(videos[video])
+        self.sequences = []  # contains train and valid sequences
+        # todo place the stuff below into a function with a good name
+        for video in videos_train.keys():  # todo look up whether .keys returns sorted list
+            video_len = len(videos_train[video])
 
             start_index = 0
             while True:
                 sequence = np.zeros([seq_length, 120, 5])  # 120 boxes with 5 parameter
                 if start_index + seq_length < video_len:
-                    sub_sequence = videos[video][start_index: start_index + seq_length]
+                    sub_sequence = videos_train[video][start_index: start_index + seq_length]
                     for j in range(seq_length):
                         sequence[j, :sub_sequence[j].shape[0], :] = sub_sequence[j][:, 1:6]
+                    ## test
+                    # assume ped id starts at 1
+                    ped_ids_from_first_frame = sequence[0, np.where(sequence[0, :, 0] != 0), 0]
+                    not_remaining_ped_idx = np.logical_not(np.isin(sequence[:, :, 0], ped_ids_from_first_frame))
+                    sequence[not_remaining_ped_idx] = 0
+                    ## test
                     self.sequences.append(sequence)
                     start_index += step
                 else:
                     print("finished video {} at index {}/{}".format(video, start_index+seq_length, video_len))
+                    break
+        self.valid_begin = len(self.sequences)  # important for datasplit with data.subset
+        for video in videos_valid.keys():
+            video_len = len(videos_valid[video])
+
+            start_index = 0
+            while True:
+                sequence = np.zeros([seq_length, 120, 5])  # 120 boxes with 5 parameter
+                if start_index + seq_length < video_len:
+                    sub_sequence = videos_valid[video][start_index: start_index + seq_length]
+                    for j in range(seq_length):
+                        sequence[j, :sub_sequence[j].shape[0], :] = sub_sequence[j][:, 1:6]
+
+                    # assume ped id starts at 1
+                    ped_ids_from_first_frame = sequence[0, np.where(sequence[0, :, 0] != 0), 0]
+                    not_remaining_ped_idx = np.logical_not(np.isin(sequence[:, :, 0], ped_ids_from_first_frame))
+                    sequence[not_remaining_ped_idx] = 0
+
+                    self.sequences.append(sequence)
+                    start_index += step
+                else:
+                    print("finished video {} at index {}/{}".format(video, start_index + seq_length, video_len))
                     break
 
         # print stats
@@ -147,11 +186,12 @@ if __name__ == "__main__":
     # debug
     test_data = MotBBSequence('../Mot17_test_single.txt')
     # draw boxes in black image
-    image = np.zeros((224, 224), dtype=np.uint8)
     test_sequence = test_data[0][0]
     print(test_sequence.shape)
     for i in range(19):
         boxes = test_sequence[i]
+        image = np.zeros((416, 416), dtype=np.uint8)
+
         for box in range(120):
             if np.sum(boxes[box, 1:]) != 0:
                 width = int(boxes[box, 3])
