7150c695fa589cf46fbbd5af15135ff50425a453
Cram13 <marc.katzenmaier@gmx.de>
2018-11-29 21:24:18 +0100

    test training with colab

--- train_yolo_example.py
+++ train_yolo_example.py
@@ -59,9 +59,9 @@ def train(opt):
     else:
         torch.manual_seed(123)
 
-    opt.image_size = 96  #todo hardcoded bad
+    opt.image_size = 416  #todo hardcoded bad
     dataset_file = 'dataset_utils/Mot17_test_single.txt'
-    dataset_file = 'dataset_utils/Mot17_1_video.txt'
+    #dataset_file = 'dataset_utils/Mot17_1_video.txt'
     trans = torchvision.transforms.Compose([torchvision.transforms.Resize((opt.image_size, opt.image_size))])
     training_set = MOT_bb_singleframe(dataset_file, transform=trans)
     training_loader = DataLoader(training_set, shuffle=True)
@@ -73,14 +73,18 @@ def train(opt):
     opt.log_path = './log/test'
     opt.pre_trained_model_path = './models/yolo80_coco.pt'
     # save convention: https://pytorch.org/tutorials/beginner/saving_loading_models.html
-
-    model = Yolo(training_set.num_classes)
+    load_strict = False
+    model = Yolo(0, anchors=[(6.88, 27.44), (11.93, 58.32), (19.90, 94.92), (40.00, 195.84), (97.96, 358.62)])
     if torch.cuda.is_available() and useCuda:
         device = torch.device("cuda")
-        model.load_state_dict(torch.load(opt.pre_trained_model_path, map_location="cuda:0"))
+        model_state_dict = torch.load(opt.pre_trained_model_path, map_location="cuda:0")
+        #model.load_state_dict(torch.load(opt.pre_trained_model_path, map_location="cuda:0"), strict=load_strict)
     else:
         device = torch.device('cpu')
-        model.load_state_dict(torch.load(opt.pre_trained_model_path, map_location='cpu'))
+        model_state_dict = torch.load(opt.pre_trained_model_path, map_location='cpu')
+        #model.load_state_dict(torch.load(opt.pre_trained_model_path, map_location='cpu'), strict=load_strict)
+    del model_state_dict["stage3_conv2.weight"]  # todo hacki
+    model.load_state_dict(model_state_dict, strict=load_strict)
     model.to(device)
 
     # The following line will re-initialize weight for the last layer, which is useful
@@ -119,8 +123,12 @@ def train(opt):
             loss, loss_coord, loss_conf = criterion(logits, gt)
             loss.backward()
             optimizer.step()
-            print('Iteration {0:7d} loss: {1:8.4f}, \tcoord_loss: {2:8.4f}, \tconf_loss: {3:8.4f}'.format(epoch*epoch_len + img_nr, loss.detach().cpu().numpy(), loss_coord.detach().cpu().numpy(),
-                                                         loss_conf.detach().cpu().numpy()))
+            if img_nr % 100 == 0:
+                print('Iteration {0:7d} loss: {1:8.4f}, \tcoord_loss: {2:8.4f}, \tconf_loss: {3:8.4f}'
+                      .format(epoch*epoch_len + img_nr,
+                              loss.detach().cpu().numpy(),
+                              loss_coord.detach().cpu().numpy(),
+                              loss_conf.detach().cpu().numpy()))
 
             writer.add_scalar('Train/Total_loss', loss, epoch*epoch_len + img_nr)
             writer.add_scalar('Train/Coordination_loss', loss_coord, epoch*epoch_len + img_nr)
