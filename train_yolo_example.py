31d9b5c9b8548952318adebd36ecc4f3746a49dd
Cram13 <marc.katzenmaier@gmx.de>
2018-11-29 20:41:05 +0100

    modified gitignore and training example

--- train_yolo_example.py
+++ train_yolo_example.py
@@ -60,8 +60,8 @@ def train(opt):
         torch.manual_seed(123)
 
     opt.image_size = 96  #todo hardcoded bad
-    dataset_file = '/home/marc/Documents/projects/ADL4CV_project/ADL4CV_Project/dataset_utils/Mot17_test_single.txt'
-    dataset_file = '/home/marc/Documents/projects/ADL4CV_project/ADL4CV_Project/dataset_utils/Mot17_1_video.txt'
+    dataset_file = 'dataset_utils/Mot17_test_single.txt'
+    dataset_file = 'dataset_utils/Mot17_1_video.txt'
     trans = torchvision.transforms.Compose([torchvision.transforms.Resize((opt.image_size, opt.image_size))])
     training_set = MOT_bb_singleframe(dataset_file, transform=trans)
     training_loader = DataLoader(training_set, shuffle=True)
@@ -71,7 +71,7 @@ def train(opt):
 
     # load the model
     opt.log_path = './log/test'
-    opt.pre_trained_model_path = '.models/yolo80_coco.pt'
+    opt.pre_trained_model_path = './models/yolo80_coco.pt'
     # save convention: https://pytorch.org/tutorials/beginner/saving_loading_models.html
 
     model = Yolo(training_set.num_classes)
@@ -89,7 +89,7 @@ def train(opt):
     nn.init.normal_(list(model.modules())[-1].weight, 0, 0.01)
 
     # log stuff
-    log_path = os.path.join(opt.log_path, "{}".format(opt.year))
+    log_path = opt.log_path
     if os.path.isdir(log_path):
         shutil.rmtree(log_path)
     os.makedirs(log_path)
@@ -152,92 +152,11 @@ def train(opt):
         writer.add_scalar('Test/Confidence_loss', te_conf_loss, epoch)
         torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict()},
-                   opt.log_path+'/snapshot{:4d}.tar'.format(epoch))
+                   opt.log_path+'/snapshot{:04d}.tar'.format(epoch))
 
-
-    writer.export_scalars_to_json(log_path + os.sep + "all_logs.json")
+    #writer.export_scalars_to_json(log_path + os.sep + "all_logs.json")
     writer.close()
     #torch.save(model.state_dict(), '/home/marc/Documents/projects/ADL4CV_project/models/my_test_model/model_state_dict.pt')
-
-    '''# actual training
-    num_iter_per_epoch = len(training_generator)
-    for epoch in range(opt.num_epoches):
-        if str(epoch) in learning_rate_schedule.keys():
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = learning_rate_schedule[str(epoch)]
-        for iter, batch in enumerate(training_generator):
-            image, label = batch
-            if torch.cuda.is_available() and useCuda:
-                image = Variable(image.cuda(), requires_grad=True)
-            else:
-                image = Variable(image, requires_grad=True)
-            optimizer.zero_grad()
-            logits = model(image)
-            loss, loss_coord, loss_conf, loss_cls = criterion(logits, label)
-            loss.backward()
-            optimizer.step()
-            print("Epoch: {}/{}, Iteration: {}/{}, Lr: {}, Loss:{:.2f} (Coord:{:.2f} Conf:{:.2f} Cls:{:.2f})".format(
-                epoch + 1,
-                opt.num_epoches,
-                iter + 1,
-                num_iter_per_epoch,
-                optimizer.param_groups[0]['lr'],
-                loss,
-                loss_coord,
-                loss_conf,
-                loss_cls))
-            writer.add_scalar('Train/Total_loss', loss, epoch * num_iter_per_epoch + iter)
-            writer.add_scalar('Train/Coordination_loss', loss_coord, epoch * num_iter_per_epoch + iter)
-            writer.add_scalar('Train/Confidence_loss', loss_conf, epoch * num_iter_per_epoch + iter)
-            writer.add_scalar('Train/Class_loss', loss_cls, epoch * num_iter_per_epoch + iter)
-        if epoch % opt.test_interval == 0:
-            model.eval()
-            loss_ls = []
-            loss_coord_ls = []
-            loss_conf_ls = []
-            loss_cls_ls = []
-            for te_iter, te_batch in enumerate(test_generator):
-                te_image, te_label = te_batch
-                num_sample = len(te_label)
-                if torch.cuda.is_available() and useCuda:
-                    te_image = te_image.cuda()
-                with torch.no_grad():
-                    te_logits = model(te_image)
-                    batch_loss, batch_loss_coord, batch_loss_conf, batch_loss_cls = criterion(te_logits, te_label)
-                loss_ls.append(batch_loss * num_sample)
-                loss_coord_ls.append(batch_loss_coord * num_sample)
-                loss_conf_ls.append(batch_loss_conf * num_sample)
-                loss_cls_ls.append(batch_loss_cls * num_sample)
-            te_loss = sum(loss_ls) / test_set.__len__()
-            te_coord_loss = sum(loss_coord_ls) / test_set.__len__()
-            te_conf_loss = sum(loss_conf_ls) / test_set.__len__()
-            te_cls_loss = sum(loss_cls_ls) / test_set.__len__()
-            print("Epoch: {}/{}, Lr: {}, Loss:{:.2f} (Coord:{:.2f} Conf:{:.2f} Cls:{:.2f})".format(
-                epoch + 1,
-                opt.num_epoches,
-                optimizer.param_groups[0]['lr'],
-                te_loss,
-                te_coord_loss,
-                te_conf_loss,
-                te_cls_loss))
-            writer.add_scalar('Test/Total_loss', te_loss, epoch)
-            writer.add_scalar('Test/Coordination_loss', te_coord_loss, epoch)
-            writer.add_scalar('Test/Confidence_loss', te_conf_loss, epoch)
-            writer.add_scalar('Test/Class_loss', te_cls_loss, epoch)
-            model.train()
-            if te_loss + opt.es_min_delta < best_loss:
-                best_loss = te_loss
-                best_epoch = epoch
-                # torch.save(model, opt.saved_path + os.sep + "trained_yolo_coco")
-                torch.save(model.state_dict(), opt.saved_path + os.sep + "only_params_trained_yolo_coco")
-                torch.save(model, opt.saved_path + os.sep + "whole_model_trained_yolo_coco")
-
-            # Early stopping
-            if epoch - best_epoch > opt.es_patience > 0:
-                print("Stop training at epoch {}. The lowest loss achieved is {}".format(epoch, te_loss))
-                break
-    writer.export_scalars_to_json(log_path + os.sep + "all_logs.json")
-    writer.close()'''
 
 
 if __name__ == "__main__":
